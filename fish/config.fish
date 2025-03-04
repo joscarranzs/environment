ba4cc2616927903cf5ee10ef3654980486528cff
joscarranzs <jcarranzsosa@gmail.com>
2025-03-03 21:46:02 -0500

    A FISH function was added to create projects in Maven

--- fish/config.fish
+++ fish/config.fish
@@ -34,3 +34,49 @@ set PATH $PATH /home/jcarranzs/.local/bin
 
 # Added by LM Studio CLI (lms)
 set -gx PATH $PATH /home/jcarranzs/.lmstudio/bin
+
+# Function to create a new Maven project
+function maven-project --description "Create a new Maven project"
+    # Default variables
+    set project_name ""
+    set package_name ""
+
+    # Process the arguments
+    for arg in $argv
+        if string match -q -- "--name=*" $arg
+            set project_name (string replace -- "--name=" "" $arg)
+        else if string match -q -- "--package=*" $arg
+            set package_name (string replace -- "--package=" "" $arg)
+        else
+            echo "Unknown argument: $arg"
+            return 1
+        end
+    end
+
+    # Validate that required arguments are provided
+    if test -z "$project_name"
+        echo "Error: The --name argument is required."
+        return 1
+    end
+
+    if test -z "$package_name"
+        echo "Error: The --package argument is required."
+        return 1
+    end
+
+    # Create the Maven project
+    echo "Creating Maven project..."
+    mvn archetype:generate \
+        -DgroupId=$package_name \
+        -DartifactId=$project_name \
+        -DarchetypeArtifactId=maven-archetype-quickstart \
+        -DinteractiveMode=false
+
+    # Check if the command was successful
+    if test $status -eq 0
+        echo "Project '$project_name' created successfully in the current directory."
+    else
+        echo "Error creating the Maven project."
+        return 1
+    end
+end
